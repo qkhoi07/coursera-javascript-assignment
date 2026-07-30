0285fbf6415db20bfe54d871a535aee37d1433e5
qkhoi07 <quangkhoi8585@gmail.com>
2026-07-31 00:40:21 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+avaScript
+function upDate(previewPic) {
+    console.log("Alt:", previewPic.alt);
+    console.log("Source:", previewPic.src);
+
+    document.getElementById('image').innerHTML = previewPic.alt;
+    document.getElementById('image').style.backgroundImage = "url('" + previewPic.src + "')";
+}
+
+function unDo() {
+    document.getElementById('image').style.backgroundImage = "url('')";
+    document.getElementById('image').innerHTML = "Hover over an image below to display here.";
+}
